5077ba0d703f04a7e5f9b504c82d1214c859c2d6
casper <ycasperinho@gmail.com>
2018-06-07 00:35:33 +0300

    add gulp task for webpack

---
+++ gulp/webpack.js
@@ -0,0 +1,105 @@
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const server = require('./server.js');
+const argv = require('yargs').argv;
+const webpack = require('webpack');
+const rupture = require('rupture');
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+var plugins = {
+    production: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }),
+        new UglifyJSPlugin
+    ],
+    development: [
+
+    ]
+}
+
+var coffeePath = '../resources/assets/coffee/';
+
+var env = argv.dev ? 'development' : 'production';
+
+var webpackCommonConfig = {
+    resolve: {
+        extensions: ['.coffee', '.vue', '.js'],
+        modules: [
+            'resources/assets/coffee',
+            'node_modules',
+        ],
+        alias: {
+            Components: path.resolve(__dirname, coffeePath + 'components/'),
+            Mixins: path.resolve(__dirname, coffeePath + 'utils/mixins/'),
+            Utils: path.resolve(__dirname, coffeePath + 'utils/'),
+            Portal: path.resolve(__dirname, coffeePath + 'app/portal/components/'),
+            Front: path.resolve(__dirname, coffeePath + 'app/front/components/')
+        }
+    },
+    plugins: plugins[env].concat([
+        new webpack.optimize.CommonsChunkPlugin({name:'common'})
+    ]),
+    module: {
+        rules: [
+            {
+                test: /\.coffee$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
+                    },
+                    'coffee-loader'
+                ]
+            },
+            {
+                test: /\.vue$/, loader: 'vue-loader',
+                options: {
+                    postcss: [autoprefixer()]
+                }
+            }
+        ]
+    },
+    devtool: argv.dev ? 'cheap-inline-source-map' : false
+};
+
+var webpackCallback = function(err, stats) {
+    console.log(stats.toString({
+        colors: true,
+        modules: false
+    }));
+
+    server.reload();
+};
+
+gulp.task('coffee', function(){
+    webpack(
+        Object.assign(webpackCommonConfig, {
+            entry: {
+                index: 'app/front/index/Index'
+            },
+            output: {
+                filename: '[name].js',
+                path: path.resolve(__dirname, '../public/js')
+            },
+        })
+    , webpackCallback);
+
+    if (!argv.dev || argv.admin) {
+        webpack(
+            Object.assign(webpackCommonConfig, {
+                entry: {
+                    index: 'admin/index'
+                },
+                output: {
+                    filename: 'admin-[name].js',
+                    path: path.resolve(__dirname, '../public/js')
+                },
+            })
+        , webpackCallback);
+    }
+});
